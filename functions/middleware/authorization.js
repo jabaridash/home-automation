dc29608a5a8e955971a3684ac6563f6c374fb3c0
Jabari Dash <13109217+jabaridash@users.noreply.github.com>
2021-01-17 04:13:39 -0500

    wrong status code

--- functions/middleware/authorization.js
+++ functions/middleware/authorization.js
@@ -4,9 +4,9 @@ function authorize(req, res, next) {
   let api_key = req.headers['X-API-KEY'] || req.headers['x-api-key']
 
   if (!api_key) {
-    res.status(403).json({ message: 'Please supply header \'X-API-KEY\''})
+    res.status(404).json({ message: 'Please supply header \'X-API-KEY\''})
   } else if (api_key !== config.api.key) {
-    res.status(403).json({ message: 'Invalid X-API-KEY'})
+    res.status(404).json({ message: 'Invalid X-API-KEY'})
   } else {
     next()
   }
