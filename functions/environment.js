b81fcdfa0c7600bfdae708a4ad4ed9fc6719d7b2
Jabari Dash <13109217+jabaridash@users.noreply.github.com>
2021-01-20 03:24:11 -0500

    better environment error

--- functions/environment.js
+++ functions/environment.js
@@ -59,24 +59,13 @@ function get_environment() {
   const result = validator.validate(config, schema)
 
   if (!result.valid) {
-    console.error(JSON.stringify(result, null, 2))
-    throw new Error("Please supply a valid firebase functions config")
-  }
-
-  const phone_numbers = config.admin.phone_numbers.split(',')
-
-  if (phone_numbers.length < 1) {
-    const message = 'admin.phone_numbers should be a comma delimited string of valid phone numbers'
-    throw new Error(message)
+    throw new Error(JSON.stringify(result, null, 2))
   }
 
   return {
     application_name: config.application.name,
     api_key: config.api.key,
     twilio: config.twilio,
-    admins: {
-      phone_numbers: phone_numbers,
-    },
     email: config.email,
   }
 }
